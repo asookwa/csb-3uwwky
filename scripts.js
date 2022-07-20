34acd54e9e736d0f19c2ef408cc123cafe2123a8
Code Sandbox <hello@codesandbox.io>
2022-07-20 20:15:39 +0000

    Deployment from Codesandbox.

---
+++ scripts.js
@@ -0,0 +1,8 @@
+console.log("To infinity and beyond.");
+
+// write your JavaScript here
+function btnClick() {
+  var y = document.getElementById("rating");
+  var x = "Your rating: " + document.getElementById("content").value;
+  y.innerHTML = x;
+}
